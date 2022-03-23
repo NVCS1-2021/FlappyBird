5c59540f2666d34be51e85d8c73501421071aff7
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2022-03-23 09:54:25 -0400

    Matrix.add

--- FlappyBird/Matrix.java
+++ FlappyBird/Matrix.java
@@ -1,5 +1,16 @@
 public class Matrix {
-  public static void add() {
-    
+  public static float[][] add(float[][] a, float[][] b) {
+    float[][] c = new float[a.length][a[0].length];
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[0].length; j++) {
+        try {
+          c[i][j] = a[i][j] + b[i][j];
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+          System.out.println("matrices don't match");
+        }
+      }
+    }
+    return c;
   }
 }
