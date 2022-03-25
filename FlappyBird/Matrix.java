3f8a18a3bd69464bbbf34e1e4bfc4eca6b704766
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2022-03-25 07:55:44 -0400

    Matrix Math (#1)
    * start Matrix.mult
    
    * Matrix.mult
    
    * Matrix.genRandom

--- FlappyBird/Matrix.java
+++ FlappyBird/Matrix.java
@@ -13,4 +13,35 @@ public class Matrix {
     }
     return c;
   }
+  
+  public static float[][] mult(float[][] a, float[][] b) {
+    if (a[0].length != b.length) {
+      throw new RuntimeException("matrix shapes don't match");
+    }
+    float[][] c = new float[a.length][b[0].length];
+    for (int i = 0; i < c.length; i++) {
+      for (int j = 0; j < c[0].length; j++) {
+        float sum = 0.0f;
+        for (int k = 0; k < a[0].length; k++) {
+          float num = a[i][k] * b[k][j];
+          sum += num;
+        }
+        c[i][j] = sum;
+      }
+    }
+    return c;
+  }
+  
+  public static float[][] genRandMatrix(int rows, int cols) {
+    float[][] mat = new float[rows][cols];
+    //traverse through mat matrix
+    for (int i = 0; i < mat.length; i++) {
+      for (int j = 0; j < mat[0].length; j++) {
+        //set each element to Math.random()
+        mat[i][j] = (float) Math.random() * 4 - 2;
+      }
+    }
+    //return answer
+    return mat;
+  }
 }
